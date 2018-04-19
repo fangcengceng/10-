4576b9cb11bedffd3c13b98c872a3bef3c03b370
fang <fang@fangdeMacBook-Pro.local>
2018-04-19 21:55:34 +0800

    Tibia

--- 10月份/10.20/CarsListSwift/Pods/Target Support Files/Pods-CarsListSwift/Pods-CarsListSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CarsListSwift : NSObject
-@end
-@implementation PodsDummy_Pods_CarsListSwift
-@end
--- 10月份/fcc2/Pods/Target Support Files/Pods-Weibo20/Pods-Weibo20-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Weibo20 : NSObject
-@end
-@implementation PodsDummy_Pods_Weibo20
-@end
